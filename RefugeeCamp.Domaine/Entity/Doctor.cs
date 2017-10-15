871accead78769f7251eb9d5e492af1fcb12a1a2
Jasser Ben Abdallah <jasser.benabdallah@esprit.tn>
2017-10-15 20:52:07 +0100

    Add project files.

---
+++ RefugeeCamp.Domaine/Entity/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefugeeCamp.Domaine.Entity
+{
+    public class Doctor : Staff
+    {
+        public String Specialty { get; set; }
+    }
+}
---
+++ RefugeeCamp.Domaine/Entity/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefugeeCamp.Domaine.Entity
+{
+    public class Patient : Refugee
+    {
+        public DateTime RegistrationDate { get; set; }
+
+      //  public List<Allergie> Allergies { get; set; }
+    }
+}
